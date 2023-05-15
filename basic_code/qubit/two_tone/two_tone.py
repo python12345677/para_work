5ea5ed01249b88dba6fd75aed1c4ce2cea5107b4
DESKTOP-76BP0VF\wr <827184674@qq.com>
2023-05-15 10:30:14 +0800

    5-15 上午，量子点的two-tone完成

--- basic_code/qubit/two_tone/two_tone.py
+++ basic_code/qubit/two_tone/two_tone.py
@@ -14,17 +14,17 @@ rho11 = (sigmaz()+1)/2
 rho22 = rho11-sz
 rho12 = sigmap()
 
-A = 1
+A = 0.1
 eps = 0
 t = 5
-w1 = 10
-H0 = (eps+0)/2*sz + t/2*sx
-H1 = [H0,[sz,'{0}*cos({1}*t)'.format(A,w1)]]
+w1 = 5
+H0 = (eps+0)/2*sx + t/2*sz
+H1 = [H0,[sx,'{0}*cos({1}*t)'.format(A,w1)]]
 
 
 psi0 = basis(2,0)
 N = 100000
-tmax = 10
+tmax = 1000
 Fs = int(N/tmax)
 tlist = np.linspace(0,tmax,N)
 
@@ -32,18 +32,18 @@ res0 = mesolve(H0,psi0,tlist,[],[rho11])
 plt.subplot(1,3,1)
 # plt.plot(tlist,res0.expect[0])
 plt.plot(tlist,np.imag(res0.expect[0]))
-plt.xlim(0,1)
+# plt.ylim(0,1)
 
 # psi0 = basis(2,0)
 # tlist = np.linspace(0,10,N)
 res1 = mesolve(H1,psi0,tlist,[],[rho11])
 plt.subplot(1,3,2)
 plt.plot(tlist,res1.expect[0])
-plt.xlim(0,1)
+# plt.ylim(0,1)
 
 
 
-y = res0.expect[0]
+y = res1.expect[0]
 # y = np.imag(res0.expect[0])
 N = len(y)
 fft_y=fftshift(fft(y))                          #快速傅里叶变换
@@ -53,7 +53,7 @@ abs_y=np.abs(fft_y)                # 取复数的绝对值，即复数的模(双
 normalization_y=abs_y/N            #归一化处理（双边频谱）dd
 plt.subplot(1,3,3)
 plt.plot(x,normalization_y)
-plt.xlim(-50,50)
+plt.xlim(-1,1)
 rhs_clear()
 plt.show()
 
